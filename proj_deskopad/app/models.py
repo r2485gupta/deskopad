59335b034afd9e745d8909af2f28be374f44b871
daspiyush0 <daspiyush0@outlook.com>
2016-10-01 10:14:12 +0530

    Landing Page Template added, DB design added

--- proj_deskopad/app/models.py
+++ proj_deskopad/app/models.py
@@ -6,10 +6,16 @@ class location(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return str(self.location_name)
+
 class user_type(models.Model):
 	type_name = models.CharField(max_length=255)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = model.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.type_name)
 
 class user_details(models.Model):
 	location = models.ForeignKey(location)
@@ -28,11 +34,17 @@ class user_details(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = model.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return str(self.location) + str(self.contact_number)
+
 class space_category(models.Model):
 	category_name = models.CharField(max_length=255)
 	category_description = models.CharField(max_length=255)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = model.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.category_name)
 
 class space(models.Model):
 	space_name = models.CharField(max_length=255)
@@ -49,6 +61,9 @@ class space(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = model.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return str(self.space_name) + str(self.space_location)
+
 class reviews(models.Model):
 	space_id = models.ForeignKey(space)
 	reviewer_id = models.ForeignKey(User)
@@ -58,12 +73,20 @@ class reviews(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = model.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return str(self.space_id) + str(self.reviewer_id)
+
 class amenities(models.Model):
 	space_id = models.ForeignKey(space)
 	amenity_name = models.CharField(max_length=255)
 	available = models.IntegerField()
 
+	def __str__(self):
+		return str(self.space_id) + str(self.amenity_name) + str(self.available)
+
 class favourites(models.Model):
 	space_id = models.ForeignKey(space)
 	user_id = models.ForeignKey(User)
 
+	def __str__(self):
+		return str(self.space_id) + str(self.user_id)
